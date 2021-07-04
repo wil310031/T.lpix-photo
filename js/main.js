b7d5ab9492df7d586fe59774bc06dedd77f98bad
wil310031 <wil310031@hotmail.fr>
2021-07-04 20:53:30 +0200

    ass javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let texte = "Photographie";
+let timer;
+let cmpt = 0;
+let currrent;
+
+function typewriter() {
+  spanid = document.getElementById("anim");
+  if (cmpt < texte.length) {
+    currrent = spanid.innerHTML.substring(0, spanid.innerHTML.length - 1);
+    currrent += texte.charAt(cmpt) + " ";
+    spanid.innerHTML = currrent;
+  } else {
+    spanid.innerHTML = "";
+    cmpt = -1;
+  }
+  cmpt++;
+  setTimeout("typewriter()", 250);
+}
+window.onload = function () {
+  typewriter();
+};
